45f841f7c54305845da6447324c20d74cbe464da
Hunter Kantner <hunterkantner@gmail.com>
2020-09-11 00:01:28 -0700

    finished password length function

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordLength;
+var confirmLength;
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    /*var password = generatePassword();
+    var passwordText = document.querySelector("#password"); 
 
-  passwordText.value = password;
+    passwordText.value = password; */
+    confirmLength = confirm("Would you like to specify a password length?")
+    if (confirmLength === true) {
+        while (true) {
+            passwordLength = parseInt(prompt("Choose a password Length between 8 and 128 characters"));
+            /* .test makes sure the string turned integer gathered above is an actual number */
+            if ( /^[0-9]+$/.test(passwordLength) && (passwordLength >= 8) && (passwordLength <= 128)) {
+                console.log('pass');
+                /* once all 3 criteria are met the loop breaks */
+                break;
+            } else {
+            console.log('fail');
+            }
+        } alert("You have selected a " + passwordLength +" character password.");
+    }
+   
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
